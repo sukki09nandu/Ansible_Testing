5f175ce18fcc2f68c1399b0165174937efb73e6c
sukki09nandu <55177350+sukki09nandu@users.noreply.github.com>
2026-01-22 15:38:28 -0500

    modified CIDR

--- 9.vpc-peering.tf
+++ 9.vpc-peering.tf
@@ -32,7 +32,7 @@ resource "aws_route" "peering-to-ansible-vpc" {
 
 resource "aws_route" "peering-from-ansible-vpc" {
   route_table_id            = data.aws_route_table.ansible_vpc_rt.id
-  destination_cidr_block    = "172.60"
+  destination_cidr_block    = "172.60.0.0/16"
   vpc_peering_connection_id = aws_vpc_peering_connection.ansible-vpc-peering.id
   #depends_on                = [aws_route_table.terraform-public]
 }
